30397c3d5108d25bef696fa1d084e1df10b2fd0d
Scott L. Maxwell <scott.maxwell@rammount.com>
2025-01-04 15:38:50 -0500

    Update main to use sprite groups for update and draw.

--- main.py
+++ main.py
@@ -3,14 +3,19 @@
 # throughout this file
 import pygame
 from constants import *
-from player import *
+from player import Player
 
 def main():
   pygame.init()
   screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
   clock = pygame.time.Clock()
-  player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
   dt = 0
+  
+  updatable = pygame.sprite.Group()
+  drawable = pygame.sprite.Group()
+  Player.containers = (updatable, drawable)
+
+  player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
   while True:
     for event in pygame.event.get():
@@ -18,12 +23,16 @@ def main():
         return
       
     # Update our screen objects
-    player.update(dt)
+    for u in updatable:
+      u.update(dt)
     
     # Display a Black Universe filled with our objects
     screen.fill("black")
-    player.draw(screen)
-    pygame.display.flip()  # display update
+    for d in drawable:
+      d.draw(screen)
+
+    # Update the display
+    pygame.display.flip()
 
     # Limit the framerate to 60 FPS
     dt = clock.tick(60) / 1000
