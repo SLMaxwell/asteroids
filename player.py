8eef8923559feeaec2ef6e4b4a8f0b5e9388be04
Scott L. Maxwell <scott.maxwell@rammount.com>
2025-01-04 17:48:48 -0500

    Update the players shot to fire with a cooldown.

--- player.py
+++ player.py
@@ -21,6 +21,9 @@ class Player(CircleShape):
         pygame.draw.polygon(screen, "white", self.triangle(), 2)
     
     def update(self, dt):
+        if self.shot_timer > 0:
+            self.shot_timer -= dt
+
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_a] or keys[pygame.K_LEFT]:
@@ -32,7 +35,9 @@ class Player(CircleShape):
         if keys[pygame.K_s] or keys[pygame.K_DOWN]:
             self.move(-dt)
         if keys[pygame.K_SPACE]:
-            self.shoot()
+            if self.shot_timer <= 0:
+                self.shoot()
+                self.shot_timer = PLAYER_SHOOT_COOLDOWN
 
     def rotate(self, dt):
         self.rotation += (PLAYER_TURN_SPEED * dt)
